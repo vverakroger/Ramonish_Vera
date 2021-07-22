e902ee480afa33f81ac4c778141193e5367bbd4c
Valeria <valeria2251997@example.com>
2021-07-22 13:58:58 -0300

    JQuery Cambios no subidos

--- js/lesson.js
+++ js/lesson.js
@@ -14,17 +14,28 @@ const cardsImgs = ["card1Img", "card2Img", "card3Img"];
 const cardsText = ["card1H4", "card2H4", "card3H4"];
 const lessonss = [lesson1, lesson2, lesson3];
 
-for (let i = 0 ; i < lessonss.length; i++) {
-    // Carga de imágenes
-    var img = document.createElement("img");
-    img.src = lessonss[i].Image;
-    var src = document.getElementById(cardsImgs[i]);
-    src.appendChild(img);
 
-    //Carga de texto
-    document.getElementById(cardsText[i]).innerHTML = lessonss[i].Text;
+// Carga de imágenes    JQuery
+for (let i = 0; i < lessonss.length; i++) {
+    $(document).ready(function() {
+        var url = lessonss[i].Image;
+        var image = new Image();
+        image.src = url;
+        var j = i + 1;
+        var cardId = '#card' + j + 'Img'
+        $(cardId).append(image);
+    });
 }
 
+//var img = document.createElement("img");
+//img.src = lessonss[i].Image;
+//var src = document.getElementById(cardsImgs[i]);
+//src.appendChild(img);
+
+//Carga de texto
+document.getElementById(cardsText[i]).innerHTML = lessonss[i].Text;
+
+//Alert JQuery
 jQuery(document).ready(function($) {
     $(".button1").click(function(){
         alert("The paragraph was clicked.");
